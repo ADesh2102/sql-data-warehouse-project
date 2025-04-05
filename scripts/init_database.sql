8fbff94fa39a1b243133f16be088fc7040a654aa
ADesh2102 <anaghag07@gmail.com>
2025-04-05 13:02:10 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,21 @@
 
+**NOTE:**
+This script will:
+- Drop the existing `DataWarehouse` database if it exists.
+- Recreate a fresh `DataWarehouse` database.
+- Create the following schemas inside it: `bronze`, `silver`, and `gold`.
+
+**WARNING:**
+Running this script will permanently delete all existing data, tables, and objects inside `DataWarehouse`.  
+Use this only for development or testing purposes.  
+**Do not use in production** unless you're absolutely sure!
+\c postgres
+
+DROP DATABASE IF EXISTS "DataWarehouse";
+CREATE DATABASE "DataWarehouse";
+
+\c "DataWarehouse"
+
+CREATE SCHEMA IF NOT EXISTS bronze;
+CREATE SCHEMA IF NOT EXISTS silver;
+CREATE SCHEMA IF NOT EXISTS gold;
